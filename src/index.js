1cc42587e064579983b6d214178ed30bb815748b
Murakami Ryouta <murakami.ryouta@videomarket.co.jp>
2019-04-03 20:33:42 +0900

    フォームの一部を実装

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-alert("hello");
+console.log('Hello, World!')
+
+var vue_example = new Vue({
+  el: '#vue_example',
+
+  template: `<div>
+    <p>{{ message }}</p>
+    <input v-model="message" />
+    <button v-on:click="reverse()">Reverse</button>
+    </div>`,
+
+  data: {
+    message: 'Hello Vue.js!',
+  },
+
+  methods: {
+    reverse: function () {
+      this.message = this.message.split('').reverse().join('')
+    },
+  },
+})
